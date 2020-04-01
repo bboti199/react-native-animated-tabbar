4bd9585482476d08befa4152ed66e3b2c8acdf9b
bboti199 <b.boti199@yahoo.com>
2020-04-01 17:36:16 +0300

    modified styling of the tabbar

--- src/styles.ts
+++ src/styles.ts
@@ -2,9 +2,15 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
+    backgroundColor: '#101010',
     flexDirection: 'row',
     justifyContent: 'center',
     paddingHorizontal: 24,
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+    position: 'absolute',
+    right: 0,
+    left: 0,
+    bottom: 0
   },
 });
